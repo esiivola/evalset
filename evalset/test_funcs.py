2ecdffb4be0e5d98910baa266bd3207a1c47bcdf
Eero Siivola <eero.siivola@aalto.fi>
2020-03-30 10:55:55 +0300

    Added few functions that are derived from real data

--- evalset/test_funcs.py
+++ evalset/test_funcs.py
@@ -94,13 +94,44 @@ def execute_random_search(num_fevals, num_trials, function):
 
     return f_best_hist
 
+class BlackBoxFunction:
+    '''
+    All functions to be optimized with BayesianOptimization class
+    or DerivativeBayesianOptimization class must inherit this class
+    '''
 
-class TestFunction(object):
+    def __init__(self):
+        pass
+
+    def get_dim(self):
+        '''
+        Should return the size of the input space
+        '''
+        raise NotImplementedError
+
+    def do_evaluate(self, x):
+        '''
+        returns the possibly stochastic evaluation for given x
+        '''
+        raise NotImplementedError
+
+    def do_evaluate_clean(self, x):
+        '''
+        If possible, returns the noiseless evaluation for given x
+        '''
+        return None
+
+    def f(self, x):
+        raise NotImplementedError
+
+
+class TestFunction(BlackBoxFunction):
     """
     The base class from which functions of interest inherit.
     """
 
     __metaclass__ = ABCMeta
+
 
     def __init__(self, dim, verify=True):
         assert dim > 0
@@ -114,12 +145,40 @@ class TestFunction(object):
         self.bounds = None
         self.classifiers = []
 
-        # Note(Mike) - Not using the records yet, but will be soon
         self.records = None
+        self.noise_std = 0.0
+        self.lengths=None
+
+        self.deviation = 1.0
+        bounds_array, lengths = self.tuplebounds_2_arrays(lzip([0] * self.dim, [1] * self.dim))
+        self.us_bounds = bounds_array
+        self.lengths = lengths
         self.reset_records()
+
+    def init_normalize_Y(self):
+        self.deviation = self.fmax-self.fmin
+
+    def init_normalize_X(self):
+        bounds_array, lengths = self.tuplebounds_2_arrays(self.bounds)
+        self.lengths = lengths
+        self.bounds = lzip([0] * self.dim, [1] * self.dim)
+        self.us_bounds = bounds_array
+        self.min_loc_01 = (self.min_loc-self.us_bounds[:,0])/self.lengths
+
+    def tuplebounds_2_arrays(self, bounds):
+        bounds_array = numpy.zeros((self.dim,2))
+        lengths = numpy.zeros((self.dim))
+        for i in range(self.dim):
+            bounds_array[i,0] = bounds[i][0]
+            bounds_array[i,1] = bounds[i][1]
+            lengths[i] = bounds[i][1]- bounds[i][0]
+        return bounds_array, lengths
 
     def __repr__(self):
         return '{0}({1})'.format(self.__class__.__name__, self.dim)
+
+    def get_dim(self):
+        return self.dim
 
     def evaluate(self, x):
         if self.verify and (not isinstance(x, numpy.ndarray) or x.shape != (self.dim,)):
@@ -140,6 +199,24 @@ class TestFunction(object):
     def reset_records(self):
         self.records = {'time': [], 'locations': [], 'values': []}
 
+    def f_c(self, x):
+        '''
+        returns function values when x is given in  numpy array of shape N x d where N is number of points and d is dimension
+        '''
+        x = numpy.atleast_2d(x)
+        y = numpy.array([(self.do_evaluate_clean( (x[i,:]*self.lengths + self.us_bounds[:,0]) ) - self.fmin)/self.deviation - 1.0  for i in range(x.shape[0])])
+        return y.flatten()
+
+    def f(self, x):
+        '''
+        returns function values when x is given in  numpy array of shape N x d where N is number of points and d is dimension
+        '''
+
+        x = numpy.atleast_2d(x)
+        y = numpy.array([(self.do_evaluate_clean( (x[i,:]*self.lengths + self.us_bounds[:,0]) ) - self.fmin)/self.deviation - 1.0 \
+            + numpy.random.normal(0, self.noise_std,1) for i in range(x.shape[0])])
+        return y.flatten()
+
     @abstractmethod
     def do_evaluate(self, x):
         """
@@ -147,6 +224,14 @@ class TestFunction(object):
         :type x: numpy.array with shape (self.dim, )
         """
         raise NotImplementedError
+
+    @abstractmethod
+    def do_evaluate_clean(self, x):
+        """
+        :param x: point at which to evaluate the function
+        :type x: numpy.array with shape (self.dim, )
+        """
+        return self.do_evaluate(x)
 
 
 class Discretizer(TestFunction):
@@ -349,6 +434,9 @@ class Noisifier(TestFunction):
             self.type,
             self.level,
         )
+    
+    def do_evaluate_clean(self, x):
+        return self.func.do_evaluate_clean(x)
 
 
 class Ackley(TestFunction):
@@ -4032,3 +4120,171 @@ class Problem22(TestFunction):
     def do_evaluate(self, x):
         x = x[0]
         return exp(-3 * x) - (sin(x)) ** 3
+
+
+
+class DataFunction(TestFunction):
+    """
+    A base class for using data as a function.
+    The class takes care of setting up the bounds,
+    finding the locations of minimum and maximum 
+    and interpolating in the points in between the
+    data points
+    
+    Given the data (X) and observations (Y), forms a function that
+    interpolates the values between the provided locations
+    
+    :param X: Input locations
+    :param Y: Input observations
+    """
+    def __init__(self, X: np.ndarray, Y: np.ndarray):
+        dim = X.shape[1]
+        super(DataFunction, self).__init__(dim)
+
+        self.X = X
+        self.Y = Y
+
+        self.bounds = lzip(numpy.min(X, axis=0), numpy.max(X, axis=0))
+        self.min_loc = self.X[numpy.argmin(self.Y), :]
+
+            
+        #Append corners with score 0
+        corners = DataFunction.give_corners(self.bounds)
+
+        self.X = numpy.r_[self.X, corners]
+        self.Y = numpy.r_[self.Y, numpy.max(self.Y)*numpy.ones((corners.shape[0],1))]
+        self.interpolator = LinearNDInterpolator(self.X, self.Y, fill_value=numpy.nan)
+
+        self.fmin = numpy.min(self.Y)
+        self.fmax = numpy.max(self.Y)
+        self.classifiers = ['complicated', 'oscillatory', 'unimodal', 'noisy']
+
+    @staticmethod
+    def give_corners(bounds: np.ndarray) -> np.ndarray:
+        """
+        Given the bounds, returns the corners of the hyperrectangle the data just barely fits
+        
+        :param bounds: Bounds of the data set (minimum and maximum in every dimension)
+        :return: All corners of the dataset 
+        """
+        if len(bounds) > 1:
+            corners = DataFunction.give_corners(bounds[1:])
+            firsts = numpy.c_[numpy.ones((corners.shape[0],1))*bounds[0][0], corners]
+            seconds = numpy.c_[numpy.ones((corners.shape[0],1))*bounds[0][1], corners]
+            return numpy.r_[firsts, seconds]
+        else:
+            return numpy.array(bounds[-1]).reshape((-1,1))
+
+    def do_evaluate(self, x: np.ndarray) -> np.ndarray:
+        """
+        Evaluates the data function at the given location by interpolating
+        
+        :param x: location where the data is wanted to be interpolated
+        :return: interpolation result
+        """
+        return self.interpolator(x)
+
+class Sushi(DataFunction):
+    """
+    Sushi data as a function : http://www.kamishima.net/sushi/ 
+    
+    :params use_discrete_features: if True, also discrete features of data are used.
+    :param num_features: how many of the continuous features are used.
+    """
+    def __init__(self, use_discrete_features: bool=False, num_features: int=4):
+        # The data files:
+        dirname, _ = os.path.split(os.path.abspath(__file__))
+        df_features = pd.read_csv(os.path.realpath(os.path.join(dirname,"data-files", "sushi3.idata")), sep="\t", header=None)
+        df_scores = pd.read_csv(os.path.realpath(os.path.join(dirname,"data-files", "sushi3b.5000.10.score")), sep=" ", header=None)
+        # Select the features we want (in the order of importance defined by RBF ARD kernel)
+        features = df_features.values[:, [6,5,7,8][:num_features] ]
+        if use_discrete_features:
+            discrete_features = df_features.values[:, [2, 3, 4]]
+            features = numpy.concatenate((features, discrete_features), axis=1)
+
+        # Generate scrores from preferences
+        scores = []
+        for item_a in range(df_scores.values.shape[1]):
+            score = 0
+            for item_b in range(df_scores.values.shape[1]):
+                if Sushi._prefer_a(item_a, item_b, df_scores):
+                    score += 1
+            scores.append(score / float(df_scores.values.shape[1]))
+        
+        X = features
+        Y = - numpy.array(scores).reshape((-1,1)) # Invert, since we want to find the maximum by BO which finds the minimum
+        
+        # Scale the data between 0 and 1
+        X = (X - numpy.min(X, axis=0)[None, :])/(numpy.max(X, axis=0) - numpy.min(X, axis=0))[None, :]
+        Y = (Y-numpy.min(Y))/(numpy.max(Y)-numpy.min(Y))
+        
+        super(Sushi, self).__init__(X, Y)
+
+    @classmethod
+    def _prefer_a(cls, item_a: int, item_b: int, df_scores: List):
+        """
+        Check from data if item_a has higher score that item_b
+        
+        :param item_a: index of the first item to be compared
+        :param item_b: index of the second item to be compared
+        :param df_scores: Scores of all dat points
+        :return: True if item_a is preferred over item_b
+        """
+        ix = (df_scores[item_a].values > -1) * (df_scores[item_b].values > -1)
+        prefer_a = numpy.mean(df_scores[item_a].values[ix] > df_scores[item_b].values[ix])
+        prefer_b = numpy.mean(df_scores[item_b].values[ix] > df_scores[item_a].values[ix])
+        return prefer_a > prefer_b
+
+class Concrete(DataFunction):
+    '''
+    Concrete compressive strength data as a function: https://archive.ics.uci.edu/ml/datasets/concrete+compressive+strength 
+    
+    :param num_features: how many of the features are used.
+    '''
+    def __init__(self, num_features: int=3):
+        data_file = "data.csv"
+        importance = [2, 0, 3, 7, 1, 4, 5, 6]
+        
+        dirname, _ = os.path.split(os.path.abspath(__file__))
+        data = pd.read_csv(os.path.realpath(os.path.join(dirname,"data-files", data_file)), sep=",", header=None)                
+
+        X = data.values[:,0:-1]
+        Y = -data.values[:,-1].reshape((-1,1))
+        X = (X - numpy.min(X, axis=0)[None, :])/(numpy.max(X, axis=0) - numpy.min(X, axis=0))[None, :]
+        Y = (Y-numpy.min(Y))/(numpy.max(Y)-numpy.min(Y))
+
+        X = X[:, importance[:num_features]]
+        super(Concrete, self).__init__(X, Y)
+
+class Candy(DataFunction):
+    '''
+    Halloween candy data as a function: https://fivethirtyeight.com/features/the-ultimate-halloween-candy-power-ranking/ 
+    '''
+    def __init__(self):
+        data_file = "candy-data.csv"
+        dirname, _ = os.path.split(os.path.abspath(__file__))
+        X = pd.read_csv(os.path.realpath(os.path.join(dirname,"data-files", data_file)), sep=",", header=None)
+        
+        Y = -X.values[:,-1].reshape((-1,1))        
+        X = X.values[:,-3:-1]
+        
+        X = (X - numpy.min(X, axis=0)[None, :])/(numpy.max(X, axis=0) - numpy.min(X, axis=0))[None, :]
+        Y = (Y-numpy.min(Y))/(numpy.max(Y)-numpy.min(Y))
+        super(Candy, self).__init__(X, Y)
+
+class Wine(DataFunction):
+    '''
+    White wine quality data as a function: https://archive.ics.uci.edu/ml/datasets/Wine+Quality 
+    
+    :param num_features: how many of the features are used.
+    '''
+    def __init__(self, num_features: int=4):
+        data_file = "wine_data.csv"
+        importance = [ 5, 3, 7, 6, 10, 4, 1, 8, 2, 0, 9] 
+
+        dirname, _ = os.path.split(os.path.abspath(__file__))
+        data = pd.read_csv(os.path.realpath(os.path.join(dirname,"data-files", data_file)), sep=",", header=None)
+        X = data.values[:,0:-1]
+        Y = -data.values[:,-1].reshape((-1,1))+1.0
+        X = X[:, importance[:num_features]]
+        super(Wine, self).__init__(X, Y)
